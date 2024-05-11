7300ec106f9820c67487ce749dee315453a4bd14
Mushi1221 <115475520+Mushi1221@users.noreply.github.com>
2024-05-11 19:11:52 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
 st.title('Profile')
-st.subheader('Nama : Adam Dwi Maulana')
-st.subheader('NIM : 312210242')
+st.subheader('Nama : Muhammad Shiddiq)
+st.subheader('NIM : 312210125')
 st.subheader('Kelas : TI.22.B1')
 st.subheader('Prodi : Teknik Infomatika')
 
-st.image("./img/adam3.jpg", caption="Adam", use_column_width="Always", width=200)
 
-
-st.link_button("Instagram", "https://www.instagram.com/adam_webdev/")
-st.link_button("Github", "https://github.com/adam-webdev")
+st.link_button("Instagram", "https://www.instagram.com/shiddiqxd/")
+st.link_button("Github", "https://github.com/Mushi1221")
